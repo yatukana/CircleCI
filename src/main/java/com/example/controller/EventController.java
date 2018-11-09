93ed30ef0b7e99dfa39502af2705533d9ace17b6
yatukana <t-tokanai@swallows-nest.co.jp>
2018-11-09 22:16:30 +0900

    EclipseからIntellijへの設定変更（動くようにする修正）

--- src/main/java/com/example/controller/EventController.java
+++ src/main/java/com/example/controller/EventController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.example.model.Event;
 import com.example.service.EventService;
 
 @RestController
+@MapperScan("com.example.mapper")
 public class EventController {
 
 	@Autowired
