6a9f02e398faee2ebe5035f9671c8b521e291f02
yatukana <t-tokanai@swallows-nest.co.jp>
2018-11-14 23:19:58 +0900

    Fortune.javaの修正

--- src/main/java/com/example/model/Fortune.java
+++ src/main/java/com/example/model/Fortune.java
@@ -3,24 +3,30 @@ package com.example.model;
 
 import com.example.entity.FortuneEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Fortune {
 
-    private String uid;
+    private String wordOne;
 
-    private String guid;
+    private String wordTwo;
 
-    private String summary;
-
-    private Integer folderId;
+    private String wordThree;
 
     @JsonIgnore
     public void toEvent(FortuneEntity entity) {
-        this.setUid(entity.getUid());
-        this.setFolderId(entity.getFolderId());
-        this.setSummary(entity.getSummary());
+        this.setWordOne(entity.getWordOne());
+        this.setWordTwo(entity.getWordTwo());
+        this.setWordThree(entity.getWordThree());
     }
 
 }
